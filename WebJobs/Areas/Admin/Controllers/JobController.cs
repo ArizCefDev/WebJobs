27ffd035c10e9c1248cbe4614721b1c33d9f79bf
ArizCefDev <106917027+ArizCefDev@users.noreply.github.com>
2022-09-08 11:27:42 +0400

    Adding Admin panel

---
+++ WebJobs/Areas/Admin/Controllers/JobController.cs
@@ -0,0 +1,22 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebJobs.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class JobController : Controller
+    {
+        private readonly IJobsService _jobsService;
+
+        public JobController(IJobsService jobsService)
+        {
+            _jobsService = jobsService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _jobsService.GetList();
+            return View(values);
+        }
+    }
+}
---
+++ WebJobs/Areas/Admin/Controllers/MessageController.cs
@@ -0,0 +1,22 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebJobs.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MessageController : Controller
+    {
+        private readonly IMessageService _messageService;
+
+        public MessageController(IMessageService messageService)
+        {
+            _messageService = messageService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _messageService.GetList();
+            return View(values);
+        }
+    }
+}

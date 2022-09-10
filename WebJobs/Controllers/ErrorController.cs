aabdd1000d89d28e3e3e24efe9a977cd900edd94
ArizCefDev <106917027+ArizCefDev@users.noreply.github.com>
2022-09-10 14:49:15 +0400

    Create error page

---
+++ WebJobs/Controllers/ErrorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebJobs.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public IActionResult Error1(int code)
+        {
+            return View();
+        }
+    }
+}
--- WebJobs/Startup.cs
+++ WebJobs/Startup.cs
@@ -58,6 +58,7 @@ namespace WebJobs
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseStatusCodePagesWithReExecute("/Error/Error1", "?code={0}");
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
